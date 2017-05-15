8625e03f1d2694ab79507edfda39558c55ceee20
Marina <maridigiolo@gmail.com>
2017-05-15 14:59:18 -0500

    datacommit

--- psql_exe/restaurant.sql
+++ psql_exe/restaurant.sql
@@ -17,31 +17,65 @@ CREATE TABLE restaurant (
 # --- updating data ---
 -- UPDATE restaurant SET favorite_dish = 'chicken la madeleine' WHERE id = 6;
 
-# --- queries to get info ---
 
--- # --- ex1
--- SELECT name FROM restaurant WHERE stars = 5;
---
--- # --- ex2
--- SELECT name, favorite_dish FROM restaurant WHERE stars = 5;
---
--- # --- ex3
--- SELECT name, favorite_dish FROM restaurant WHERE stars = 5; (**)
---
--- # --- ex4
--- SELECT name FROM restaurant WHERE category = 'health';
---
--- # --- ex5
--- SELECT name FROM restaurant WHERE takeout = TRUE;
---
--- # --- ex6
--- SELECT name FROM restaurant WHERE takeout = TRUE AND category = 'health';
---
--- # --- ex7
--- SELECT name FROM restaurant WHERE distance < 20;
---
--- # --- ex8
--- SELECT name FROM restaurant WHERE last_visit < '2017-05-07';
---
--- # --- ex9
--- SELECT name FROM restaurant WHERE last_visit < '2017-05-07' AND stars = 5;
+ < Writing Queries >--
+
+-- 1.The names of the restaurants that you gave a 5 stars to
+SELECT name FROM restaurant WHERE stars = 5;
+
+-- 2.The favorite dishes of all 5-star restaurants
+SELECT name, favorite_dish FROM restaurant WHERE stars = 5;
+
+-- 3.The the id of a restaurant by a specific restaurant name, say 'Moon Tower'
+SELECT name, id FROM restaurant WHERE name = 'Moon Tower';
+
+-- 4.restaurants in the category of 'BBQ'
+SELECT name FROM restaurant WHERE category = 'health';
+
+-- 5.restaurants that do take out
+SELECT name FROM restaurant WHERE takeout = TRUE;
+
+-- 6.restaurants that do take out and is in the category of 'BBQ'
+SELECT name FROM restaurant WHERE takeout = TRUE AND category = 'health';
+
+-- 7.restaurants within 2 miles
+SELECT name FROM restaurant WHERE di\qstance < 20;
+
+-- 8.restaurants you haven't ate at in the last week
+SELECT name FROM restaurant WHERE last_visit < '2017-05-07';
+
+-- 9.restaurants you haven't ate at in the last week and has 5 stars
+SELECT name FROM restaurant WHERE last_visit < '2017-05-07' AND stars = 5;
+
+
+--< Aggregation and Sorting Queries >--
+
+-- 1.list restaurants by the closest distance
+SELECT * FROM restaurant ORDER BY distance;
+
+-- 2.ist the top 2 restaurants by distance
+SELECT * FROM restaurant ORDER BY distance limit 2;
+
+-- 3.list the top 2 restaurants by stars
+SELECT * FROM restaurant ORDER BY stars DESC limit 2;
+
+-- 4.list the top 2 restaurants by stars where the distance is less than 2 miles
+SELECT * FROM restaurant  WHERE distance < 20 ORDER BY stars DESC limit 2;
+
+-- 5.count the number of restaurants in the db
+SELECT COUNT(*) FROM restaurant;
+
+-- 6.count the number of restaurants by category
+SELECT category,
+  COUNT(*) AS num_restaurant
+FROM restaurant GROUP BY category;
+
+-- 7.get the average stars per restaurant by category
+SELECT category,
+  AVG(stars) AS average_stars
+FROM restaurant GROUP BY category;
+
+-- 8.get the max stars of a restaurant by category
+SELECT category,
+  MAX(stars) AS max_stars
+FROM restaurant GROUP BY category;
